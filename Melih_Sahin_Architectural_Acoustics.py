d207eed732f7ca09a0c9a76082d7494df08219ab
MelihSahinEdu <92105399+MelihSahinEdu@users.noreply.github.com>
2024-03-10 21:23:59 +0300

    Update Melih_Sahin_Architectural_Acoustics.py

--- Melih_Sahin_Architectural_Acoustics.py
+++ Melih_Sahin_Architectural_Acoustics.py
@@ -1,3 +1,20 @@
+"""
+Copyright 2022 Melih Şahin
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+
 # Google Server Link: https://colab.research.google.com/drive/1TLhIFcfBY22xS6SxXSd1rPHcSadqzeOp?usp=sharing#scrollTo=EaVGKRDIRUvW
 #Room Geometry Impulse Response Generator:
 #Melih Şahin Koç University Summer 2022 Research Internship under the guiadnce of Professor Dr. Engin Erzin
